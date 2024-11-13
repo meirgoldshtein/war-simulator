dfbf14c31dae0d7a806ae9857db2590a70e25a7d
meirgoldshtein <m0548491057@gmail.com>
2024-11-13 10:30:43 +0200

    Initialize a main file to create an express server and a socket service

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,31 @@
-console.log("server is running")
+console.log('server start running');
+import express from 'express';
+import 'dotenv/config';
+import { connectDB } from './config/db';
+import cors from 'cors';
+import http from 'http';
+import { Server } from 'socket.io';
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors());
+connectDB();
+app.use(express.json());
+
+app.use('/api/users', (req, res, next) => {
+    
+});
+
+export const io = new Server(server,{ cors: { origin: "*" } });
+io.on('connection', (socket) => {
+    console.log('socket: a user connected');
+    
+    socket.on('disconnect', () => {
+        console.log('socket: a user disconnected');
+    });
+});
+
+server.listen(PORT, () => console.log(`Listening on port ${PORT},visit http://localhost:${PORT}`));
