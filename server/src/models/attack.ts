7fb75cba2e3697c3fe7d38b460ca3e257a53f076
meirgoldshtein <m0548491057@gmail.com>
2024-11-13 12:16:35 +0200

    Adding a model of attack management

---
+++ server/src/models/attack.ts
@@ -0,0 +1,32 @@
+import { Document, model, ObjectId, Schema, Types } from "mongoose";
+
+enum Status {
+    LAUNCHED = "LAUNCHED",
+    HIT = "HIT",
+    INTERCEPTED = "INTERCEPTED"
+}
+
+export interface IAttack extends Document {
+    rocket: string,
+    launchTime: Date,
+    timeToHit: number,
+    status: Status,
+    orgSrc: string,
+    distLocation: string,
+}
+
+const attackSchema = new Schema<IAttack>({
+    rocket: {
+        type: String,
+    },
+    launchTime: { type: Date, required: true },
+    timeToHit: { type: Number, required: true },
+    status: {
+        type: String,
+        default: Status.LAUNCHED
+    },
+    orgSrc: { type: String, required: true },
+    distLocation: { type: String, required: true },
+})
+
+export default model<IAttack>("Attack", attackSchema);
